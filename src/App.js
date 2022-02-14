3be0758aad6ae7132d4554e52cdae8853d47806e
Conor Kelly <conorjkelly96@gmail.com>
2022-02-14 20:05:28 +0000

    IntroCard component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { useState } from "react";
 import { Header } from "./Components/Header";
+import { IntroCard } from "./Components/IntroCard";
 
 export const App = () => {
   return (
     <div className="App">
       <Header title="Conor Kelly | Oracle Consultant & Full Stack Developer" />
+      <section>
+        <IntroCard />
+      </section>
     </div>
   );
 };
---
+++ src/Components/IntroCard/index.js
@@ -0,0 +1,47 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
+const bull = (
+  <Box
+    component="span"
+    sx={{ display: "inline-block", mx: "2px", transform: "scale(0.8)" }}
+  >
+    •
+  </Box>
+);
+
+const card = (
+  <React.Fragment>
+    <CardContent>
+      <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+        Hi there! 👋 I'm
+      </Typography>
+      <Typography variant="h5" component="div">
+        Conor Kelly
+      </Typography>
+      <Typography sx={{ mb: 1.5 }} color="text.secondary">
+        Oracle Senior Consultant & Software Developer 👨‍💻
+      </Typography>
+      <Typography variant="body2">
+        Applying my background in business management with full stack technical
+        skills to solve real world problems 📊 💾 🖥️
+      </Typography>
+    </CardContent>
+    <CardActions>
+      <Button size="small">Learn More</Button>
+    </CardActions>
+  </React.Fragment>
+);
+
+export const IntroCard = () => {
+  return (
+    <Box sx={{ minWidth: 275 }}>
+      <Card variant="outlined">{card}</Card>
+    </Box>
+  );
+};
