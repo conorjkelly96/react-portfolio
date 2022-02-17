2c780a1a8a17c81bd3c3007001144f4e1a06f8fc
Conor Kelly <conorjkelly96@gmail.com>
2022-02-17 22:08:47 +0000

    import app

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 import { AppRoutes } from "./AppRoutes";
 import { NavBar } from "./components/NavBar";
+import { Home } from "./pages/Home";
 
 export const App = () => {
   return (
     <Router>
       <NavBar />
       <AppRoutes />
+      <Home />
     </Router>
   );
 };
---
+++ src/pages/Home/index.js
@@ -0,0 +1,11 @@
+import { IntroCard } from "../../components/IntroCard";
+import { MemojiCard } from "../../components/MemojiCard";
+
+export const Home = () => {
+  return (
+    <div>
+      <IntroCard />
+      <MemojiCard />
+    </div>
+  );
+};
