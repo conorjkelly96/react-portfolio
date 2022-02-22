2571f8cc706e5bb24bf96356d39c758380e9b4ec
Conor Kelly <conorjkelly96@gmail.com>
2022-02-22 21:46:39 +0000

    GNU GENERAL PUBLIC LICENSE

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 import { AppRoutes } from "./AppRoutes";
 import { NavBar } from "./components/NavBar";
-import { Home } from "./pages/Home";
 
 export const App = () => {
   return (
