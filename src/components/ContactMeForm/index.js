7026e80d8967ada8e7f3ede49ea4329dc0102b27
Conor Kelly <conorjkelly96@gmail.com>
2022-02-27 13:44:36 +0000

    wip

--- src/components/ContactMeForm/index.js
+++ src/components/ContactMeForm/index.js
@@ -1,36 +1,46 @@
-import * as React from "react";
+import { useState } from "react";
+
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
-import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
-import Link from "@mui/material/Link";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import ContactMailIcon from "@mui/icons-material/ContactMail";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { CssBaseline, TextField } from "@mui/material";
+import { SubmitModal } from "../SubmitModal";
 
 const theme = createTheme();
 
 export const ContactMeForm = () => {
+  const [showNoBackEndModal, setNoBackEndModal] = useState(false);
+
+  const handleClose = () => setNoBackEndModal(false);
+
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-    // eslint-disable-next-line no-console
     console.log({
+      firstName: data.get("firstName"),
+      lastName: data.get("lastName"),
       email: data.get("email"),
-      password: data.get("password"),
+      description: data.get("description"),
     });
+  };
+
+  const onSubmitContactMeForm = () => {
+    console.log("submitted form");
+    setNoBackEndModal(true);
   };
 
   return (
     <ThemeProvider theme={theme}>
       <Container component="main" maxWidth="xs">
         <CssBaseline />
+        <SubmitModal show={showNoBackEndModal} onClose={handleClose} />
         <Box
+          onSubmit={handleSubmit}
           sx={{
             marginTop: 8,
             display: "flex",
@@ -47,7 +57,7 @@ export const ContactMeForm = () => {
           <Box
             component="form"
             noValidate
-            onSubmit={handleSubmit}
+            // onSubmit={handleSubmit}
             sx={{ mt: 3 }}
           >
             <Grid container spacing={2}>
@@ -98,6 +108,7 @@ export const ContactMeForm = () => {
               fullWidth
               variant="contained"
               sx={{ mt: 3, mb: 2 }}
+              onSubmit={onSubmitContactMeForm}
             >
               Submit
             </Button>
