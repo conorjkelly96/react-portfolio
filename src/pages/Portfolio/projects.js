23bd46030719c5876c09e8572172843604cd05a9
Conor Kelly <conorjkelly96@gmail.com>
2022-02-27 11:19:14 +0000

    project cards

--- src/pages/Portfolio/projects.js
+++ src/pages/Portfolio/projects.js
@@ -1,6 +1,8 @@
 import { makeStyles } from "@material-ui/core/styles";
 
-import project1 from "./images/Stockscape.png";
+import stockscape from "./images/Stockscape.png";
+import nosqlapi from "./images/no-sql-api.gif";
+import jscodequiz from "./images/jscodequiz.gif";
 
 export const useStyles = makeStyles({
   cardContainer: {
@@ -19,7 +21,28 @@ export const projects = [
       "bcrypt, chart.js, session-sequelize, express, handlebars.js, express.js, mySQL, sequelize ORM",
     github: "https://github.com/conorjkelly96/stockscape",
     app: "https://stockscape.herokuapp.com/",
-    image: project1,
+    image: stockscape,
+  },
+  {
+    title: "Social Network API - NoSQL",
+    description:
+      "A RESTful API for a social network web application where users can share their thoughts, react to friends’ thoughts, and create a friend list, leveraging Express.js for routing, a MongoDB database, and the Mongoose ODM.",
+    technologies: "JavaScript",
+    tools: "Apollo-Server, Express.js, GraphQL, Moment, Mongoose",
+    github:
+      "https://github.com/conorjkelly96/social-network-api-nosql/tree/main",
+    app: "https://github.com/conorjkelly96/social-network-api-nosql/tree/main",
+    image: nosqlapi,
+  },
+  {
+    title: "JavaScript Code Quiz",
+    description:
+      "The goal for this project is to construct a JavaScript quiz which demonstrates dynamic DOM manipulation skills. The site functionality relies heavily on user interaction mainly via answer selection and user information. As the user progresses through the quiz, the time remaining on the quiz is dynamically updated, reflecting the user's score.",
+    technologies: "JavaScript, HTML, CSS",
+    tools: "Google Fonts, Custom CSS",
+    github: "https://github.com/conorjkelly96/javascript-code-quiz",
+    app: "https://conorjkelly96.github.io/javascript-code-quiz/",
+    image: jscodequiz,
   },
   {
     title: "StockScape",
@@ -30,7 +53,7 @@ export const projects = [
       "bcrypt, chart.js, session-sequelize, express, handlebars.js, express.js, mySQL, sequelize ORM",
     github: "https://github.com/conorjkelly96/stockscape",
     app: "https://stockscape.herokuapp.com/",
-    image: project1,
+    image: nosqlapi,
   },
   {
     title: "StockScape",
@@ -41,7 +64,7 @@ export const projects = [
       "bcrypt, chart.js, session-sequelize, express, handlebars.js, express.js, mySQL, sequelize ORM",
     github: "https://github.com/conorjkelly96/stockscape",
     app: "https://stockscape.herokuapp.com/",
-    image: project1,
+    image: nosqlapi,
   },
   {
     title: "StockScape",
@@ -52,7 +75,7 @@ export const projects = [
       "bcrypt, chart.js, session-sequelize, express, handlebars.js, express.js, mySQL, sequelize ORM",
     github: "https://github.com/conorjkelly96/stockscape",
     app: "https://stockscape.herokuapp.com/",
-    image: project1,
+    image: nosqlapi,
   },
   {
     title: "StockScape",
@@ -63,7 +86,7 @@ export const projects = [
       "bcrypt, chart.js, session-sequelize, express, handlebars.js, express.js, mySQL, sequelize ORM",
     github: "https://github.com/conorjkelly96/stockscape",
     app: "https://stockscape.herokuapp.com/",
-    image: project1,
+    image: nosqlapi,
   },
   {
     title: "StockScape",
@@ -74,7 +97,7 @@ export const projects = [
       "bcrypt, chart.js, session-sequelize, express, handlebars.js, express.js, mySQL, sequelize ORM",
     github: "https://github.com/conorjkelly96/stockscape",
     app: "https://stockscape.herokuapp.com/",
-    image: project1,
+    image: nosqlapi,
   },
   {
     title: "StockScape",
@@ -85,7 +108,7 @@ export const projects = [
       "bcrypt, chart.js, session-sequelize, express, handlebars.js, express.js, mySQL, sequelize ORM",
     github: "https://github.com/conorjkelly96/stockscape",
     app: "https://stockscape.herokuapp.com/",
-    image: project1,
+    image: nosqlapi,
   },
   {
     title: "StockScape",
@@ -96,7 +119,7 @@ export const projects = [
       "bcrypt, chart.js, session-sequelize, express, handlebars.js, express.js, mySQL, sequelize ORM",
     github: "https://github.com/conorjkelly96/stockscape",
     app: "https://stockscape.herokuapp.com/",
-    image: project1,
+    image: nosqlapi,
   },
   {
     title: "StockScape",
@@ -107,7 +130,7 @@ export const projects = [
       "bcrypt, chart.js, session-sequelize, express, handlebars.js, express.js, mySQL, sequelize ORM",
     github: "https://github.com/conorjkelly96/stockscape",
     app: "https://stockscape.herokuapp.com/",
-    image: project1,
+    image: nosqlapi,
   },
   {
     title: "StockScape",
@@ -118,28 +141,6 @@ export const projects = [
       "bcrypt, chart.js, session-sequelize, express, handlebars.js, express.js, mySQL, sequelize ORM",
     github: "https://github.com/conorjkelly96/stockscape",
     app: "https://stockscape.herokuapp.com/",
-    image: project1,
-  },
-  {
-    title: "StockScape",
-    description:
-      "A full-stack application leveraging an express server, mySQL database, handlebars templates, javascript, and chart.js to create an interactive stock market themed game.",
-    technologies: "JavaScript, HTML, CSS",
-    tools:
-      "bcrypt, chart.js, session-sequelize, express, handlebars.js, express.js, mySQL, sequelize ORM",
-    github: "https://github.com/conorjkelly96/stockscape",
-    app: "https://stockscape.herokuapp.com/",
-    image: project1,
-  },
-  {
-    title: "StockScape",
-    description:
-      "A full-stack application leveraging an express server, mySQL database, handlebars templates, javascript, and chart.js to create an interactive stock market themed game.",
-    technologies: "JavaScript, HTML, CSS",
-    tools:
-      "bcrypt, chart.js, session-sequelize, express, handlebars.js, express.js, mySQL, sequelize ORM",
-    github: "https://github.com/conorjkelly96/stockscape",
-    app: "https://stockscape.herokuapp.com/",
-    image: project1,
+    image: nosqlapi,
   },
 ];
