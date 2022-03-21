704d27b602fa08dbd281b687b5aa1dbd2ab6ca82
Conor Kelly <conorjkelly96@gmail.com>
2022-03-21 11:24:08 +0000

    basename

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter } from "react-router-dom";
+import { Router } from "react-router-dom";
 
 import { AppRoutes } from "./AppRoutes";
 import { Footer } from "./components/Footer";
@@ -6,10 +6,10 @@ import { NavBar } from "./components/NavBar";
 
 export const App = () => {
   return (
-    <HashRouter>
+    <Router basename="/react-portfolio">
       <NavBar />
       <AppRoutes />
       <Footer />
-    </HashRouter>
+    </Router>
   );
 };
